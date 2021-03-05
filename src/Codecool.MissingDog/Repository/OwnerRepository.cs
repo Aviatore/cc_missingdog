a60e9c9330e974086876a7a3652b225c88cc5115
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-03-05 08:39:53 +0100

    Initial commit

---
+++ src/Codecool.MissingDog/Repository/OwnerRepository.cs
@@ -0,0 +1,89 @@
+using System;
+using Codecool.MissingDog.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codecool.MissingDog.Repository
+{
+    /// <summary>
+    ///     Provides all needed methods on owners data.
+    /// </summary>
+    public class OwnerRepository
+    {
+        private DataSource _data { get; }
+
+        /// <summary>
+        ///     Initiates a new instance of OwnerRepository based on given DataSource.
+        /// </summary>
+        /// <param name="data"></param>
+        public OwnerRepository(DataSource data)
+        {
+            _data = data;
+        }
+
+        /// <summary>
+        ///     Gets all owners from data.
+        /// </summary>
+        /// <returns> IEnumerable of all Owners instances and nulls. </returns>
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return _data.Owners;
+        }
+
+        /// <summary>
+        ///     Gets specific owner with given Id.
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns> Owner instance or null. </returns>
+        public Owner GetOwnerById(int id)
+        {
+            return _data.Owners.FirstOrDefault(o => o?.Id == id);
+        }
+
+        /// <summary>
+        ///     Gets count of dogs of specific breed that this owner has.
+        /// </summary>
+        /// <param name="ownerId">ownerId</param>
+        /// <param name="breedId">breedId</param>
+        /// <returns> Integer, representing count of Dogs. </returns>
+        public int GetCountOfDogsOfSpecificBreedThisOwnerHas(int breedId, int ownerId)
+        {
+            return GetOwnerById(ownerId)?.Dogs?.Count(dog => dog?.Breed?.Id == breedId) ?? 0;
+        }
+
+        /// <summary>
+        ///     Counts an average dogs activitie levels of owner's with given Id or returns zero.
+        /// </summary>
+        /// <param name="ownerId">ownerId</param>
+        /// <returns> Double, representing average level. </returns>
+        public double CountAverageActivityLevelOfThisOwnersDogs(int ownerId)
+        {
+            return GetOwnerById(ownerId)?
+                .Dogs?
+                .Where(d => (d?.Breed?.ActivityLevel).HasValue)
+                .Select(d => d.Breed.ActivityLevel).Average() ?? 0;
+        }
+
+        /// <summary>
+        ///     Gets the oldest dog of owner with given Id or returns null when none.
+        /// </summary>
+        /// <param name="ownerId">ownerId</param>
+        /// <returns> Dog instance or null </returns>
+        public Dog GetOldestDogOfThisOwner(int ownerId)
+        {
+            return GetOwnerById(ownerId)?.Dogs?.Where(d => d?.DateOfBirth != null).OrderBy(d => d.DateOfBirth)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        ///     Count dogs of owner with given Id.
+        /// </summary>
+        /// <param name="ownerId">ownerId</param>
+        /// <returns> Integer, representing Dogs count. </returns>
+        public int CountDogsOfThisOwners(int ownerId)
+        {
+            return GetOwnerById(ownerId)?.Dogs?.Count(d => d != null) ?? 0;
+        }
+    }
+}
+
